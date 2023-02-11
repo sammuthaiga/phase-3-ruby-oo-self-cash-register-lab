add176322d4893e30ae31ec7ae16ba0bd43bc775
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-12 00:25:56 +0300

    Completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,29 +1,35 @@
 class CashRegister
-  attr_accessor :total
+    attr_accessor :total, :quantity, :items, :price
+    attr_reader :discount, :title
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
+    def initialize(discount=0, total = 0)
+        @total = total
+        @discount=discount
+        @items = []
+    end
 
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    quantity.times { @items << title }
-  end
+    def add_item(title, price, quantity=1)
+        @title = title
+        @price = price * quantity
+        quantity.times { @items << title }
+        self.total += self.price
+    end
 
-  def apply_discount
-    return "There is no discount to apply." if @discount == 0
-    @total *= (100 - @discount) / 100.0
-    "After the discount, the total comes to $#{@total.round(2)}."
-  end
+    def apply_discount
+        if @discount > 0
+            @total -= (@total * @discount/100)
+            "After the discount, the total comes to $#{@total.round(2)}."
+        else
+            "There is no discount to apply."
+        end
+    end
 
-  def items
-    @items
-  end
+    def items
+        @items
+    end
 
-  def void_last_transaction
-    last_item = @items.pop
-    @total -= @items.count(last_item) * @total / @items.count
-  end
+    def void_last_transaction
+        @total -= @price
+    end
+
 end
